ed9bf77d64a4b3ae3768730aacd543ade962afb6
Marcos Crispim <99744094+marcoscrispim@users.noreply.github.com>
2022-11-22 18:51:20 -0300

    Add files via upload

---
+++ Maior_ou_menor.rb
@@ -0,0 +1,16 @@
+puts "Bm vindo ao Jogo de adivinhação"
+puts "Qual o seu nome?"
+nome = gets 
+puts "Comecaremos o jogo pra você, " + nome
+
+puts "Escolhendo um número secreto entre 0 e 200"
+numero_secreto = 45
+puts "Escolhido... adivinhe"
+
+puts
+puts
+puts
+puts "Tentativa 1"
+puts "entre com o numero"
+chute = gets 
+puts "Será que acertou? você chutou " + chute
