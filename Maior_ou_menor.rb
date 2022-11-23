f8c877173d1503ab4fee480c6b23d2c503bb0f4a
Marcos Crispim <99744094+marcoscrispim@users.noreply.github.com>
2022-11-23 11:57:45 -0300

    Update Maior_ou_menor.rb

--- Maior_ou_menor.rb
+++ Maior_ou_menor.rb
@@ -1,16 +1,61 @@
-puts "Bm vindo ao Jogo de adivinhação"
-puts "Qual o seu nome?"
-nome = gets 
-puts "Comecaremos o jogo pra você, " + nome
+def da_boas_vindas
+    puts "Bem vindo ao jogo da adivinhação"
+    puts "Qual é o seu nome?"
+    nome = gets
+    puts "\n\n\n\n\n\n"
+    puts "Começaremos o jogo para você, " + nome
+end
 
-puts "Escolhendo um número secreto entre 0 e 200"
-numero_secreto = 45
-puts "Escolhido... adivinhe"
+def sorteia_numero_secreto
+    puts "Escolhendo um número secreto entre 0 e 200..."
+    sorteado = 175
+    puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+    sorteado
+end
 
-puts
-puts
-puts
-puts "Tentativa 1"
-puts "entre com o numero"
-chute = gets 
-puts "Será que acertou? você chutou " + chute
+def pede_um_numero(chutes, tentativa, limite_de_tentativas)
+    puts "\n\n\n\n"
+    puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+    puts "Chutes até agora: " + chutes.to_s
+    puts "Entre com o número"
+    chute = gets
+    puts "Será que acertou? Você chutou " + chute
+    chute.to_i
+end
+
+
+def verifica_se_acertou(numero_secreto, chute)
+    acertou = numero_secreto == chute
+    if acertou
+        puts "Acertou!"
+        return true
+    end
+
+    maior = numero_secreto > chute
+    if maior
+        puts "O número secreto é maior!"
+    else
+        puts "O número secreto é menor!"
+    end
+    false
+end
+
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+
+limite_de_tentativas = 5
+chutes = []
+
+
+for tentativa in 1..limite_de_tentativas
+   
+    chute = pede_um_numero chutes, tentativa, limite_de_tentativas
+    chutes << chute
+
+    if verifica_se_acertou numero_secreto, chute
+        break
+    end
+end
+
